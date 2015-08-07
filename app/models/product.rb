b4a70ac8b35c4e1291852dd4e0c1f6dc84194b56
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-08-07 12:34:23 +0200

    format price and order product

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,8 @@ class Product < ActiveRecord::Base
   }
   validates :price, numericality: {greater_than_or_equal_to: 0.01 }
 
-  
+  def self.latest
+    Product.order(:updated_at).last
+  end
 
 end
