772a6defc9ad74060f2f8f3ad6fd8dc2bdd389ac
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-08-08 12:42:59 +0200

    adding dependencies

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
@@ -11,5 +14,17 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 
 end
