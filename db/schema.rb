b9fd5ff54eac1b485440752ae71192cc5d7a3378
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-08-10 20:25:44 +0200

    combine items cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810170059) do
+ActiveRecord::Schema.define(version: 20150810174137) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
