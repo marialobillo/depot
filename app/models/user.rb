8d602e5da4f9cd69a2b35ae192c0313a005947f1
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-08-19 17:48:22 +0200

    users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+  after_destroy :ensure_an_admin_remains
+
   has_secure_password
 
   validates :name, presence: true, uniqueness: true
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+    end
 end
